1439e26de4d89ac113f3e2bf5bb47b0bdab3e50d
Dwiyer <dwiyer@DwiyerdeMacBook-Pro.local>
2015-05-24 08:49:53 +0800

    initiate the project.

---
+++ MobileHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileHelper
+//
+//  Created by Dwiyer on 5/24/15.
+//  Copyright (c) 2015 Dwiyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileHelper
+//
+//  Created by Dwiyer on 5/24/15.
+//  Copyright (c) 2015 Dwiyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
